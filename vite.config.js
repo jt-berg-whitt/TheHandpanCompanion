decfe7d0a28505bdb57af52264fa7218bc17264d
jt-berg-whitt <jt@berg-whitt.com>
2025-11-05 11:49:14 -0500

    typo fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/TheHandpanCompanion/',
+  base: '/thehandpan-companion-website/',
   plugins: [react()],
 });
