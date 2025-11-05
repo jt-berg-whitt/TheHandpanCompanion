41fd2b5d7cc2b1135d745d3988035abdd4998c3d
jt-berg-whitt <jt@berg-whitt.com>
2025-11-05 11:19:41 -0500

    added missing vite dependency

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/thehandpan-companion-website/', // your GitHub repo name
+  base: '/thehandpan-companion-website/',
   plugins: [react()],
 });
