1b11943220bb877f99859c2e4dc555c363f970ae
jt-berg-whitt <jt@berg-whitt.com>
2025-11-05 23:47:23 -0500

    fixed vite base url

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 export default defineConfig({
-  base: process.env.VITE_BASE_URL,
+  base: isDev ? '/' : (process.env.VITE_BASE_URL || '/'),
   plugins: [react()],
 });
